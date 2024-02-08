ded37c4ec7ffccba7c905dbff4a5c665a6babf85
Yehor Pavliukov <yehor_pavliukov@epam.com>
2024-02-08 19:55:48 +0200

    implementation main menu

---
+++ lib/menu/view/main_menu_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_game_challenge/common.dart';
+import 'package:flutter_game_challenge/game/game.dart';
+import 'package:flutter_game_challenge/title/title.dart';
+
+class MainMenuPage extends StatelessWidget {
+  const MainMenuPage({super.key});
+
+  static Route<void> route() {
+    return MaterialPageRoute<void>(
+      builder: (_) => const MainMenuPage(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MenuItem(
+                text: l10n.mainMenuGamesItemTitle,
+                onTap: () {
+                  Navigator.of(context).push(GamePage.route());
+                },
+              ),
+              MenuItem(
+                text: l10n.mainMenuArtifactsItemTitle,
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MenuItem extends StatelessWidget {
+  const MenuItem({
+    required this.text,
+    required this.onTap,
+    super.key,
+  });
+
+  final String text;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    const style = TextStyle(
+      fontSize: 50,
+    );
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Text(
+        text,
+        style: style,
+      ),
+    );
+  }
+}
