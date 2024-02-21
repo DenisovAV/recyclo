64cb40bfc7d9aec3e67bb9defc419adea8d11895
vikrech <viktor.krechetov@ah.nl>
2024-02-21 11:18:17 +0200

    Fixed issue with catcher assets; Added game timer component; Added finish dialog;

--- lib/catcher_game/catcher_game_page.dart
+++ lib/catcher_game/catcher_game_page.dart
@@ -1,12 +1,15 @@
 import 'package:flame/game.dart' hide Route;
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_game_challenge/catcher_game/game.dart';
+import 'package:flutter_game_challenge/common.dart';
 import 'package:flutter_game_challenge/menu/view/main_menu_page.dart';
+import 'package:flutter_game_challenge/service_provider.dart';
 
 class CatcherGamePage extends StatefulWidget {
   const CatcherGamePage({super.key});
 
-  static Route<void> route() {
+  static MaterialPageRoute<void> route() {
     return MaterialPageRoute<void>(
       builder: (_) => const CatcherGamePage(),
     );
@@ -24,28 +27,56 @@ class _CatcherGamePageState extends State<CatcherGamePage> {
     _game ??= CatcherGame();
 
     return Scaffold(
-      body: Stack(
-        children: [
-          GameWidget(game: _game!),
-          SafeArea(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 12),
-                  child: GameBackButton(
-                    onPressed: _handleBackButton,
-                  ),
+      body: BlocProvider<TimerCubit>(
+        create: (_) => ServiceProvider.get<TimerCubit>(),
+        child: BlocListener<TimerCubit, TimerState>(
+          listener: _handleTimerState,
+          child: Stack(
+            children: [
+              GameWidget(
+                game: _game!,
+                overlayBuilderMap: {
+                  CatcherGame.timerOverlayKey: (_, __) => const TimerOverlay(),
+                },
+              ),
+              SafeArea(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12),
+                      child: GameBackButton(
+                        onPressed: _handleBackButton,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
 
   void _handleBackButton() {
     Navigator.of(context).pop();
+  }
+
+  void _handleTimerState(BuildContext context, TimerState state) {
+    if (state == TimerFinishedState()) {
+      _game?.pauseEngine();
+      showGameFinishDialog(
+        context: context,
+        items: [
+          (ItemType.plastic, 1),
+          (ItemType.paper, 1),
+          (ItemType.glass, 1),
+        ],
+        onDismiss: () {
+          Navigator.of(context).maybePop();
+        },
+      );
+    }
   }
 }
