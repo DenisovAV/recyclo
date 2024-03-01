94f04c8207ff70d9938108a696d8e5da36b2addb
vikrech <viktor.krechetov@ah.nl>
2024-03-01 23:06:51 +0200

    Removed template code and some assets; Aligned colors for trash reserve widget;

--- lib/trash_reserve/trash_reserve_widget.dart
+++ lib/trash_reserve/trash_reserve_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_game_challenge/common/assets.dart';
+import 'package:flutter_game_challenge/clicker_game/game_models/trash_type.dart';
 import 'package:flutter_game_challenge/trash_reserve/cubit/trash_reserve_cubit.dart';
 import 'package:flutter_game_challenge/trash_reserve/cubit/trash_reserve_state.dart';
 import 'package:flutter_game_challenge/trash_reserve/trash_reserve_item_widget.dart';
@@ -16,29 +16,24 @@ class TrashReserveWidget extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           TrashReserveItemWidget(
+            type: TrashType.organic,
             isRounded: true,
-            color: FlutterGameChallengeColors.categoryGreen,
-            imagePath: Assets.images.organic.path,
             count: state.model.organic,
           ),
           TrashReserveItemWidget(
-            color: FlutterGameChallengeColors.categoryViolet,
-            imagePath: Assets.images.plastic.path,
+            type: TrashType.plastic,
             count: state.model.plastic,
           ),
           TrashReserveItemWidget(
-            color: FlutterGameChallengeColors.categoryOrange,
-            imagePath: Assets.images.glass.path,
+            type: TrashType.glass,
             count: state.model.glass,
           ),
           TrashReserveItemWidget(
-            color: FlutterGameChallengeColors.categoryYellow,
-            imagePath: Assets.images.paper.path,
+            type: TrashType.paper,
             count: state.model.paper,
           ),
           TrashReserveItemWidget(
-            color: FlutterGameChallengeColors.categoryPink,
-            imagePath: Assets.images.energy.path,
+            type: TrashType.electric,
             count: state.model.electronics,
           ),
         ],
