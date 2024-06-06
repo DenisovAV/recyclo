31ad32cb2a7c066f78951e5c05b62e10d98cb6c5
Yehor Pavliukov <yehor_pavliukov@epam.com>
2024-06-06 15:44:17 +0300

    Add keyboard handler to catcher

--- lib/catcher_game/game.dart
+++ lib/catcher_game/game.dart
@@ -4,6 +4,7 @@ import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:recyclo/catcher_game/common/size_config.dart';
 import 'package:recyclo/catcher_game/components.dart';
@@ -15,7 +16,7 @@ typedef UpdateStatusCallback = void Function(CatcherGameStatusType status);
 typedef WaveCallback = void Function(int value);
 typedef LevelCallback = void Function(int level);
 
-class CatcherGame extends FlameGame with TapCallbacks, HorizontalDragDetector {
+class CatcherGame extends FlameGame with TapCallbacks, HorizontalDragDetector, KeyboardEvents {
   CatcherGame({
     bool? isPenaltyEnabled,
     AccessibilityGameScaleType? gameScaleType,
@@ -30,10 +31,12 @@ class CatcherGame extends FlameGame with TapCallbacks, HorizontalDragDetector {
   final bool isPenaltyEnabled;
 
   late SizeConfig sizeConfig;
+
   // TODO(vikrech): replace this property with a game cubit, and subscribe to its state in children.
   late CatcherGameStatusType status;
   MainScene? mainScene;
   bool _timerStarted = false;
+  double _currentLocalPosition = 0.0;
 
   @override
   FutureOr<void> onLoad() async {
@@ -43,8 +46,7 @@ class CatcherGame extends FlameGame with TapCallbacks, HorizontalDragDetector {
     mainScene = MainScene(
       onPauseResumeGameCallback: _handlePauseResumeGameCallback,
       onResetCallback: _handleOnResetCallback,
-      assetsByItemTypeCallback:
-          ServiceProvider.get<AssetsLoader>().getAssetsListByItemType,
+      assetsByItemTypeCallback: ServiceProvider.get<AssetsLoader>().getAssetsListByItemType,
     );
 
     await add(mainScene!);
@@ -91,7 +93,66 @@ class CatcherGame extends FlameGame with TapCallbacks, HorizontalDragDetector {
 
   @override
   void handleHorizontalDragEnd(DragEndDetails details) {
-    mainScene?.onDragEnd(details);
+    mainScene?.onDragEnd();
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+    KeyEvent event,
+    Set<LogicalKeyboardKey> keysPressed,
+  ) {
+    final isKeyDown = event is KeyDownEvent;
+    final isSpace = keysPressed.contains(LogicalKeyboardKey.space);
+    final isLeft = keysPressed.contains(LogicalKeyboardKey.arrowLeft);
+    final isRight = keysPressed.contains(LogicalKeyboardKey.arrowRight);
+
+    if (isSpace) {
+      _switchBetweenPauseAndPlaying();
+      return KeyEventResult.handled;
+    }
+
+    if (isKeyDown) {
+      if (isLeft || isRight) {
+        final details = DragStartDetails(
+          localPosition: Offset(_currentLocalPosition, 0.0),
+        );
+        handleHorizontalDragStart(details);
+      }
+      if (isLeft) {
+        final updateDetails = DragUpdateDetails(
+          globalPosition: Offset.zero,
+          localPosition: Offset(_currentLocalPosition, 0.0),
+          delta: const Offset(100, 0.0),
+        );
+        _currentLocalPosition = _currentLocalPosition + 100;
+        mainScene?.onKeyBoardTap(updateDetails);
+
+        return KeyEventResult.handled;
+      }
+      if (isRight) {
+        final updatedDetails = DragUpdateDetails(
+          globalPosition: Offset.zero,
+          localPosition: Offset(_currentLocalPosition, 0.0),
+          delta: const Offset(-100, 0.0),
+        );
+        _currentLocalPosition = _currentLocalPosition - 100;
+        mainScene?.onKeyBoardTap(updatedDetails);
+
+        return KeyEventResult.handled;
+      }
+
+      return KeyEventResult.ignored;
+    }
+
+    return KeyEventResult.ignored;
+  }
+
+  void _switchBetweenPauseAndPlaying() {
+    if (status == CatcherGameStatusType.playing) {
+      _pauseGame();
+    } else if (status == CatcherGameStatusType.pause) {
+      _resumeGame();
+    }
   }
 
   void _pauseGame() {
